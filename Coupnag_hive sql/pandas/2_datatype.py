f2dedd30d1508c4afbba8a1c87152a062a752b45
Doojin Park <dopark41@coupangfs.com>
2023-02-14 21:53:32 +0900

    first commit

---
+++ Coupnag_hive sql/pandas/2_datatype.py
@@ -0,0 +1,32 @@
+# 자료형/연산자/변수
+
+# 숫자형, 문자형, 참/거짓형
+# print(1)        #자료형?       - 정수형 자료형  (숫자형 )
+# print(-1)
+#
+# print(type(1))
+
+# print(3.14)
+# print(1000000)
+# print(10+1)
+# print(3-10)
+# print(11*11)
+# print((4+1)*5)
+
+# # 변수, 속은 어떤 두 수를 x, y라 할때
+
+# x*y => 곱하기
+
+# print(3)
+# print(2)
+# print(3/2)
+#
+# print(type(2.0))
+# print(type(3))
+
+import math
+print(math.sqrt(4.))  # 2
+print(math.log10(10))  # 1
+print(math.factorial(4))  # 4! = 24
+
+pritn(a)
